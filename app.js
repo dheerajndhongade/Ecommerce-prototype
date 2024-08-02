955073a1ccdd3d053e4ae492d9a56369696392c5
dheerajndhongade <dheerajndhongade@gmail.com>
2024-08-02 21:23:22 +0530

    Wrote to the file and read from it and displayed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let http = require("http");
+let fs = require("fs");
+
 let server = http.createServer((req, res) => {
-  console.log("Dheeraj N Dhongade");
   res.setHeader("Content-type", "text/html");
   if (req.url === "/home") {
     res.write("<html>");
@@ -20,7 +21,38 @@ let server = http.createServer((req, res) => {
     res.write("<body><h1>Welcome to my Node Js project</h1></body>");
     res.write("</html>");
     return res.end();
+  } else if (req.url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" >send Message</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
   }
+  if (req.url === "/message" && req.method === "POST") {
+    let body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      let parsedData = Buffer.concat(body).toString();
+      let message = parsedData.split("=")[1];
+      console.log(parsedData);
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 server.listen("4000", () => {
   console.log("server running at port 4000");
