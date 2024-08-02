1aa5cdb60dcc931ebdce27489249ad9154ae10f0
dheerajndhongade <dheerajndhongade@gmail.com>
2024-08-02 21:43:42 +0530

    corrected code

--- app.js
+++ app.js
@@ -22,15 +22,16 @@ let server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   } else if (req.url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit" >send Message</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (req.url === "/message" && req.method === "POST") {
+    fs.readFile("message.txt", (err, data) => {
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title></head>");
+      res.write(
+        `<body><p>${data}</p><form action="/message" method="POST"><input type="text" name="message"><button type="submit" >send Message</button></form></body>`
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (req.url === "/message" && req.method === "POST") {
     let body = [];
     req.on("data", (chunk) => {
       console.log(chunk);
@@ -46,13 +47,14 @@ let server = http.createServer((req, res) => {
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title><head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("</html>");
+    res.end();
   }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
 });
 server.listen("4000", () => {
   console.log("server running at port 4000");
