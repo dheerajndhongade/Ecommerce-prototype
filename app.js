4228eebeccf43c00cdd31123760d4b7011196740
dheerajndhongade <dheerajndhongade@gmail.com>
2024-08-08 13:53:55 +0530

    modified CRUD operations using mysql2 and it's queries

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ const errorController = require("./controllers/error");
 const db = require("./util/database");
 
 const app = express();
+let port = 3000;
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
-db.execute("SELECT * FROM products");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,4 +23,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Running in ${port} ;)`);
+});
